964719112916d59c55f0e974bf1507ed01c8454c
Joey Poon <joey@joeypoon.com>
2016-03-13 23:37:55 -0500

    student :create, :show

--- app/controllers/v1/students_controller.rb
+++ app/controllers/v1/students_controller.rb
@@ -1,2 +1,27 @@
 class V1::StudentsController < ApplicationController
+  def create
+    @student = Student.new student_params
+    if @student.save!
+      render json: @student
+    else
+      render json: { error: @student.errors }, status: 422
+    end
+  end
+
+  def show
+    @student = Student.find params[:id]
+    if @student.present?
+      render json: @student
+    else
+      render json: { error: "No student with id: #{params[:id]}" }, status: 422
+    end
+  end
+
+  private
+
+    def student_params
+      params.require(:student)
+            .permit(:name, :email, :school_identifier, :phone_number,
+                    :password, :password_confirmation, phone_identifier: [])
+    end
 end
--- test/controllers/v1/students_controller_test.rb
+++ test/controllers/v1/students_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class V1::StudentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can post create' do
+    student = attributes_for :student
+    assert_difference 'Student.count' do
+      post :create, student: student.as_json
+      assert_response 200
+    end
+  end
+
+  test 'can get show' do
+    student = create :student
+    get :show, id: student.id
+    assert_response 200
+    assert_not_nil assigns :student
+  end
 end
